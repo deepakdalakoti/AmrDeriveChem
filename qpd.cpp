ec5fb6b5843f581370c69e713c0248b463af7038
Marc Day <MSDay@lbl.gov>
2016-07-06 12:49:37 -0700

    Add simple qpd func

---
+++ qpd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "ParmParse.H"
+#include "ParallelDescriptor.H"
+#include "ChemDriver.H"
+
+typedef ChemDriver::Edge Edge;
+typedef std::list<Edge> EdgeList;
+
+int
+main (int   argc,
+      char* argv[])
+{
+    BoxLib::Initialize(argc,argv);
+    ParmParse pp;
+
+    ChemDriver cd;
+    std::string QPDatom="C"; pp.query("QPDatom",QPDatom);
+    EdgeList edges = cd.getEdges(QPDatom);
+
+    for (EdgeList::const_iterator it = edges.begin(); it!=edges.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+
+    BoxLib::Finalize();
+    return 0;
+}
